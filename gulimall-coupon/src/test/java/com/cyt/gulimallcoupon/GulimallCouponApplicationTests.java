663b82ea1b12c95bd620eee7471d61185c4ee912
CYT <shaonian15769@gmail.com>
2023-11-13 14:03:22 +0800

    项目初始结构创建

---
+++ gulimall-coupon/src/test/java/com/cyt/gulimallcoupon/GulimallCouponApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cyt.gulimallcoupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallCouponApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
